5c1c17fd637c1579ad64f7aa947434a0f046c321
unleashy <unleashy@users.noreply.github.com>
2022-02-28 22:37:07 -0300

    Fix rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -5,9 +5,10 @@ import pkg from "./package.json";
 export default {
   input: "src/index.ts",
   output: [
-    { file: pkg.main, format: "cjs", exports: "auto" },
+    { file: pkg.main, format: "cjs", exports: "named" },
     { file: pkg.module, format: "es" }
   ],
+  external: [...Object.keys(pkg.dependencies), "path"],
   plugins: [
     resolve({
       extensions: [".js", ".ts"]
