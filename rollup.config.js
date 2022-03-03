6dca3e8f8a3ee3d2a221f836846e882476da9e1c
unleashy <unleashy@users.noreply.github.com>
2022-03-02 22:41:45 -0300

    Improve rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -5,8 +5,17 @@ import pkg from "./package.json";
 export default {
   input: "src/index.ts",
   output: [
-    { file: pkg.main, format: "cjs", exports: "named" },
-    { file: pkg.module, format: "es" }
+    {
+      file: pkg.main,
+      format: "cjs",
+      exports: "named",
+      generatedCode: "es2015"
+    },
+    {
+      file: pkg.module,
+      format: "es",
+      generatedCode: "es2015"
+    }
   ],
   external: [...Object.keys(pkg.dependencies), "assert", "path"],
   plugins: [
